76c5505d6928f6bcf7e51423c08ea94fa0bfc514
SunnyGit <fang_sunsf@yahoo.com>
2023-10-09 17:02:40 -0400

    finished services

--- src/app/hero/hero-details.service.ts
+++ src/app/hero/hero-details.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HeroService } from '../model/hero.service';
+import { Observable, map } from 'rxjs';
+import { Hero } from '../model/hero';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroDetailsService {
 
-  constructor() { }
+  constructor(private service: HeroService) { }
+
+  getHero(id: string | number): Observable<Hero|null> {
+    if (typeof (id) === 'string')
+      id = parseInt(id);
+
+    return this.service.getHero(id).pipe(
+      map(hero => hero? Object.assign({}, hero) : null
+      )
+    )
+  }
+
+  saveHero(hero: Hero){
+    return this.service.updateHero(hero);
+  }
+
 }
--- src/app/model/hero.service.ts
+++ src/app/model/hero.service.ts
@@ -1,9 +1,78 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError, map, tap } from 'rxjs';
+import { Hero } from './hero';
+
+const options = {
+  headers: new HttpHeaders({ 'content-Type': 'appication/json' })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  readonly apiUrl = 'api/heroes';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.apiUrl).pipe(
+      tap(heroes => { }),
+      // catchError()
+    ) as Observable<Hero[]>;
+  }
+
+  getHero<data>(id: number | string): Observable<Hero> {
+    if (typeof (id) === 'string')
+      id = parseInt(id);
+
+    const url = `${this.apiUrl}/?id=${id}`
+    return this.http.get<Hero[]>(this.apiUrl).pipe(
+      map(heroes => heroes[0]),
+      tap(),
+      //catchError({})
+    )
+  }
+
+  updateHero(hero: Hero): Observable<Hero> {
+    return this.http.put<Hero>(this.apiUrl, hero, options).pipe(
+      tap()
+    );
+  }
+
+  addHero(hero: Hero) : Observable<Hero>{
+    return this.http.post<Hero>(this.apiUrl, hero, options);
+  }
+
+  deleteHero(hero: Hero | number): Observable<Hero>{
+    const id = typeof(hero) === "number" ? hero : hero.id;
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.delete<Hero>(url, options).pipe(
+      //tap(_=> this.log(''))
+    )
+
+  }
+
+  private handleError<T>(operation = 'operation') {
+    return (error: HttpErrorResponse): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error);  // log to console instead
+
+      // If a native error is caught, do not transform it. We only want to
+      // transform response errors that are not wrapped in an `Error`.
+      if (error.error instanceof Event) {
+        throw error.error;
+      }
+
+      const message = `server returned code ${error.status} with body "${error.error}"`;
+      // TODO: better job of transforming error for user consumption
+      throw new Error(`${operation} failed: ${message}`);
+    };
+  }
+
+  private log(message: string) {
+    console.log('HeroService: ' + message);
+  }
 }
+
+
+
